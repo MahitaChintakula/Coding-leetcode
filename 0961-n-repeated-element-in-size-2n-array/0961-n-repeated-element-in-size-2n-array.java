ad89bfe28cd8c12e6192686d8ddca16d831c8866
MahitaChintakula <155607005+MahitaChintakula@users.noreply.github.com>
2026-01-02 21:53:16 +0530

    Time: 1223 ms (5.06%), Space: 47 MB (89.44%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -18,13 +18,40 @@ class Solution {
         //     }
         // }
         // return ans;
-        HashSet<Integer> map=new HashSet<>();
-        for(int num : nums){
-            if(map.contains(num)){
-                return num;
+        // HashSet<Integer> map=new HashSet<>();
+        // for(int num : nums){
+        //     if(map.contains(num)){
+        //         return num;
+        //     }
+        //     map.add(num);
+        // }
+        // return -1;
+        // for(int i=0;i<nums.length;i++){
+        //     int count=0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>(nums.length/2)){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        int max=0;
+        int ans=-1;
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
             }
-            map.add(num);
+            if(count>max){
+                max=count;
+                ans=nums[i];
+            }
         }
-        return -1;
+        return ans;
     }
 }
