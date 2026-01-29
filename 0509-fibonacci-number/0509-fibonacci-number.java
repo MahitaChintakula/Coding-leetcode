845639481dba4311ac087ee7371e5da7294bef37
MahitaChintakula <155607005+MahitaChintakula@users.noreply.github.com>
2026-01-29 21:17:55 +0530

    Time: 0 ms (100%), Space: 41.9 MB (80.02%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int f0=0,f1=1,fn=0;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        for(int i=2;i<=n;i++){
+            fn=f0+f1;
+            f0=f1;
+            f1=fn;
+        }
+        return fn;
+    }
+}
