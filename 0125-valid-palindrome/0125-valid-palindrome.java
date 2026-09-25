9791859a68b641b7775d06477c606cb746cd087b
MahitaChintakula <155607005+MahitaChintakula@users.noreply.github.com>
2026-09-25 11:53:16 +0530

    Time: 2 ms (99.3%), Space: 44.3 MB (62.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String c=s.replaceAll("[^a-zA-Z0-9]","");
-        String cs=c.toLowerCase();
-        String reversed = new StringBuilder(cs).reverse().toString();
-       if (cs.equals(reversed)) {
-         return true;
-        } 
-      return false;
+        // String c=s.replaceAll("[^a-zA-Z0-9]", "");
+        // String cs=c.toLowerCase();
+        // String rev=new StringBuilder(cs).reverse().toString();
+        // if(cs.equals(rev)){
+        //     return true;
+        // }
+        // return false;
+        int left=0;
+        int right=s.length()-1;
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
